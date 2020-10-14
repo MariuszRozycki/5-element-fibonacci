d386f9053b06238cd918dc08f162f30f6938e2e0
mariansc83 <mariansc83@icloud.com>
2020-10-14 20:21:20 +0200

    Add 5-element-fibonacci

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function fibonacci(n) {
+  
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1];
+    const b = result[i - 2];
+
+    result.push(a + b);
+  }
+
+  return result[n];
+}
+
+let fibNr = fibonacci(1000);
+console.log(fibNr); 
+
+
+
